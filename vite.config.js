3e8ec13e9c8c304328456b59e62572716691068d
Rinaldinur-14 <nurhardiansyah14@gmail.com>
2025-03-07 20:11:11 +0700

    Update website: [13]

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
   base: "/rinaldinur-portfolio/",
   assetsInclude: ["**/*.JPG"],
